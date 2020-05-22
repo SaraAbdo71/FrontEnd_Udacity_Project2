95509cad472c7368137dd9e17570fd361baa7351
Sara Abdo <sarabdo71@gmail.com>
2020-05-22 23:31:53 +0200

    create list,function to hide navbar,hide section

--- js/app.js
+++ js/app.js
@@ -17,7 +17,10 @@
  * Define Global Variables
  * 
 */
+let showThis=true;
+let li;
 
+let navSroll;
 
 /**
  * End Global Variables
@@ -25,7 +28,39 @@
  * 
 */
 
+//to hide and show section//
+function showSection ()
+{
+    if(document.getElementById('section1').click){
+        document.getElementById('section2').style.display='none' ;
+        document.getElementById('section3').style.display='none';
+    }
+    else if(document.getElementById('section2').click)
+    {
+        document.getElementById('section1').style.display='none' ;
+        document.getElementById('section3').style.display='none';
+    }
+    else{
+        document.getElementById('section1').style.display='none' ;
+        document.getElementById('section2').style.display='none';
+    }
 
+}
+showSection();
+
+/*****/
+// to hide and show NavBar//
+navSroll= window.pageXOffset;
+window.onscroll= function(){
+    let currentScroll= window.pageXOffset;
+    if(navSroll > currentScroll){
+        document.querySelector('.page__heaer').style.top ='0';
+    }
+    else {
+        document.querySelector('.page__heaer').style.top ='-50px';
+   }
+   navSroll =currentScroll;
+}
 
 /**
  * End Helper Functions
@@ -35,6 +70,33 @@
 
 // build the nav
 
+function createListItem(text) {
+    li = document.createElement('li');
+    li.textContent=text;
+    return li;
+}
+console.log(li);
+
+function appendChildren (parent, childern){
+    childern.forEach(function (child){
+        parent.appendChild(child);
+    });
+    }
+    console.log(appendChildren);
+
+    const myList =document.getElementById('navbar__list');
+    let items= [
+        createListItem=('First'),
+        createListItem=('Second'),
+        createListItem=('Third')
+    ];
+
+    appendChildren(myList,items);
+
+    createListItem.setAttribute('id','section1');
+
+    console.log( createListItem);
+    console.log(appendChildren(mylist,items));
 
 // Add class 'active' to section when near top of viewport
 
