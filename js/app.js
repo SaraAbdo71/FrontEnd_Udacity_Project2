92f6826abf5ac08b0b38f4a2be43d878401630af
Sara Abdo <sarabdo71@gmail.com>
2020-05-24 03:06:08 +0200

    Edit Functions

--- js/app.js
+++ js/app.js
@@ -1,119 +1,156 @@
 /**
- * 
+ *
  * Manipulating the DOM exercise.
  * Exercise programmatically builds navigation,
  * scrolls to anchors from navigation,
  * and highlights section in viewport upon scrolling.
- * 
+ *
  * Dependencies: None
- * 
+ *
  * JS Version: ES2015/ES6
- * 
+ *
  * JS Standard: ESlint
- * 
-*/
+ *
+ */
 
 /**
  * Define Global Variables
- * 
-*/
-let showThis=true;
+ *
+ */
+let showThis = true;
 let li;
-
-let navSroll;
+let navScroll;
 
 /**
  * End Global Variables
  * Start Helper Functions
- * 
-*/
+ *
+ */
 
 //to hide and show section//
-function showSection ()
-{
-    if(document.getElementById('section1').click){
-        document.getElementById('section2').style.display='none' ;
-        document.getElementById('section3').style.display='none';
-    }
-    else if(document.getElementById('section2').click)
-    {
-        document.getElementById('section1').style.display='none' ;
-        document.getElementById('section3').style.display='none';
-    }
-    else{
-        document.getElementById('section1').style.display='none' ;
-        document.getElementById('section2').style.display='none';
-    }
-
+function showSection() {
+  if (document.getElementById("section0").click) {
+    document.getElementById("section2").style.display = "none";
+    document.getElementById("section3").style.display = "none";
+  } else if (document.getElementById("section2").click) {
+    document.getElementById("section1").style.display = "none";
+    document.getElementById("section3").style.display = "none";
+  } else {
+    document.getElementById("section1").style.display = "none";
+    document.getElementById("section2").style.display = "none";
+  }
+  showSection();
 }
-showSection();
 
 /*****/
 // to hide and show NavBar//
-navSroll= window.pageXOffset;
-window.onscroll= function(){
-    let currentScroll= window.pageXOffset;
-    if(navSroll > currentScroll){
-        document.querySelector('.page__heaer').style.top ='0';
+navScroll = window.pageXOffset;
+var timer = null;
+
+window.onscroll = function () {
+  
+
+  var header = document.getElementsByClassName("page__header")[0];
+  if (timer !== null) {
+    clearTimeout(timer);
+  }
+  console.log(window.scrollY);
+  if (window.scrollY == 0) {
+    header.style = "display: block;";
+  } else {
+    header.style = "display: absolute;";
+    header.onmouseover = function(){
+        console.log("mouseover");
+        if (timer !== null) {
+            clearTimeout(timer);
+          }
+        this.style = "display: absolute;";
+    };
+    timer = setTimeout(function () {
+      header.style = "display: none;";
+    }, 1000);
+  }
+
+  //   navScroll = currentScroll;
+};
+
+
+
+// sections collapsible
+
+let secColla = document.getElementsByClassName("landing__container");
+let i;
+
+for (i = 0; i < secColla.length; i++) {
+  secColla[i].addEventListener("click", function () {
+    this.classList.toggle("active");
+    let content = this.nextElementSibling;
+    if (content.style.display === "block") {
+      content.style.display = "none";
+    } else {
+      content.style.display = "block";
     }
-    else {
-        document.querySelector('.page__heaer').style.top ='-50px';
-   }
-   navSroll =currentScroll;
+  });
 }
 
 /**
  * End Helper Functions
  * Begin Main Functions
- * 
-*/
+ *
+ */
 
 // build the nav
 
-function createListItem(text) {
-    li = document.createElement('li');
-    li.textContent=text;
-    return li;
+var listView = document.getElementById("navbar__list");
+var sections = document.getElementsByTagName("section");
+// Add links
+for (i = 0; i < sections.length; i++) {
+  var newList = document.createElement("li");
+  newList.innerHTML =
+    '<a href ="#" class="nav-link" data-section="' +
+    sections[i].getAttribute("id") +
+    '">' +
+    sections[i].getAttribute("id") +
+    "</a>";
+  listView.appendChild(newList);
 }
-console.log(li);
 
-function appendChildren (parent, childern){
-    childern.forEach(function (child){
-        parent.appendChild(child);
-    });
-    }
-    console.log(appendChildren);
+// Add listener
+var scrollFun = function (e) {
+  e.preventDefault();
+  var sec_id = this.getAttribute("data-section");
+  var sec = document.getElementById(sec_id);
+  sec.scrollIntoView();
+  // Set class active
+  var sections = document.getElementsByTagName("section");
+  for (var i = 0; i < sections.length; i++)
+    sections[i].classList.remove("active");
+  sec.classList.add("active");
+  
+  // window.location = "#" + sec_id;
+  return false;
+};
+//
+var elements = document.getElementsByClassName("nav-link");
+for (i = 0; i < elements.length; i++) {
+  elements[i].addEventListener("click", scrollFun, false);
+}
 
-    const myList =document.getElementById('navbar__list');
-    let items= [
-        createListItem=('First'),
-        createListItem=('Second'),
-        createListItem=('Third')
-    ];
-
-    appendChildren(myList,items);
-
-    createListItem.setAttribute('id','section1');
-
-    console.log( createListItem);
-    console.log(appendChildren(mylist,items));
 
 // Add class 'active' to section when near top of viewport
 
 
 // Scroll to anchor ID using scrollTO event
 
-
 /**
  * End Main Functions
  * Begin Events
- * 
-*/
+ *
+ */
 
-// Build menu 
+// Build menu
 
 // Scroll to section on link click
 
 // Set sections as active
-
 
